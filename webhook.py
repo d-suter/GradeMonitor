f1db821251fdd4146efcb0ad79a28cbc927b6d25
d-suter <david.raul.suter@icloud.com>
2024-05-20 16:36:19 +0200

    changes

--- webhook.py
+++ webhook.py
@@ -29,7 +29,7 @@ def send_discord_message(webhook_url, new_grades):
                 "color": 3224376,
                 "author": {
                     "name": "Grade-Monitor",
-                    "url": "https://github.com/d-suter/grade-monitor",
+                    "url": "https://github.com/d-suter/GradeMonitor",
                     "icon_url": "https://avatars.githubusercontent.com/u/93440331?s=400&u=a05cbca990d5ecce2a4be5de46181b8bebf8291f&v=4"
                 }
             }
